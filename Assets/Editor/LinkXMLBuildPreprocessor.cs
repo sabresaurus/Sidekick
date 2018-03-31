d826f2d284642b2f8aa4ed70f6b95cf932ab781a
Barnaby MacBook <barnaby@sabresaurus.com>
2018-04-01 00:30:20 +0100

    * Link.xml generation at build time - will need to be optional

---
+++ Assets/Editor/LinkXMLBuildPreprocessor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+class LinkXMLBuildPreprocessor : IPreprocessBuild
+{
+    public int callbackOrder { get { return 0; } }
+
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        Debug.Log("Preprocessing link.xml file");
+        LinkXMLFactory.Generate(LinkXMLFactory.DEFAULT_TYPES);
+    }
+}
