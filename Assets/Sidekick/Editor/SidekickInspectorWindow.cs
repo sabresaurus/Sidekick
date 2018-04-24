afab1bd4b5affd48166911901a540871e861956b
Thomas Ingram <thomasingram@vertx.xyz>
2018-04-25 09:05:35 +1000

    Improved Component foldout UI and inverted Expanded/Collapsed behaviour

--- Assets/Sidekick/Editor/SidekickInspectorWindow.cs
+++ Assets/Sidekick/Editor/SidekickInspectorWindow.cs
@@ -243,6 +243,7 @@ namespace Sabresaurus.Sidekick
 
                 foreach (var component in gameObjectResponse.Components)
                 {
+                    SidekickEditorGUI.DrawSplitter();
                     GUIStyle style = new GUIStyle(EditorStyles.foldout);
                     style.fontStyle = FontStyle.Bold;
 
@@ -252,20 +253,22 @@ namespace Sabresaurus.Sidekick
                     // Temporarily set the label width to full width so the icon is not squashed with long strings
                     EditorGUIUtility.labelWidth = position.width / 2f;
 
-                    bool wasComponentExpanded = !settings.CollapsedTypeNames.Contains(component.TypeFullName);
-                    bool isComponentExpanded = EditorGUILayout.Foldout(wasComponentExpanded, content, style);
+                    bool wasComponentExpanded = settings.ExpandedTypeNames.Contains(component.TypeFullName);
+                    bool isComponentExpanded = wasComponentExpanded;
+                    if (SidekickEditorGUI.DrawHeaderWithFoldout(content, isComponentExpanded))
+                        isComponentExpanded = !isComponentExpanded;
                     EditorGUIUtility.labelWidth = labelWidth; // Restore label width
                     if (isComponentExpanded != wasComponentExpanded)
                     {
-                        if (isComponentExpanded == false)
+                        if (isComponentExpanded)
                         {
                             // Not expanded, so collapse it
-                            settings.CollapsedTypeNames.Add(component.TypeFullName);
+                            settings.ExpandedTypeNames.Add(component.TypeFullName);
                         }
                         else
                         {
                             // Expanded, remove it from collapse list
-                            settings.CollapsedTypeNames.Remove(component.TypeFullName);
+                            settings.ExpandedTypeNames.Remove(component.TypeFullName);
                         }
                     }
 
@@ -430,13 +433,8 @@ namespace Sabresaurus.Sidekick
                             }
                         }
                     }
-                    Rect rect = GUILayoutUtility.GetRect(new GUIContent(), GUI.skin.label, GUILayout.ExpandWidth(true), GUILayout.Height(1));
-                    rect.xMin -= 10;
-                    rect.xMax += 10;
-                    GUI.color = new Color(0.5f, 0.5f, 0.5f);
-                    GUI.DrawTexture(rect, EditorGUIUtility.whiteTexture);
-                    GUI.color = Color.white;
                 }
+                SidekickEditorGUI.DrawSplitter();
             }
             EditorGUILayout.EndScrollView();
 
