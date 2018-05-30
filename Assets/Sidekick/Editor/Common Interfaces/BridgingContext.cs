bec719ab2b405f9121148c247c1caceaeff6bb1b
Barnaby MacBook <barnaby@sabresaurus.com>
2018-05-30 01:41:39 +0100

    * Added bridging context class

---
+++ Assets/Sidekick/Editor/Common Interfaces/BridgingContext.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Sabresaurus.Sidekick
+{
+    public class BridgingContext : ScriptableObject
+    {
+        [System.Serializable]
+        public class Container
+        {
+            public string a;
+            public string b;
+        }
+
+        #region Bridging
+        static Container containerStaticCopy = null;
+
+        public Container container;
+
+        private static BridgingContext instance = null;
+
+        public static BridgingContext Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = CreateInstance<BridgingContext>();
+                    instance.container = containerStaticCopy;
+                }
+                return instance;
+            }
+        }
+
+        private void OnEnable()
+        {
+            instance = this;
+        }
+
+        private void OnDisable()
+        {
+            containerStaticCopy = container;
+        }
+        #endregion
+    }
+}
