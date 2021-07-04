dff04faf6e555b59681647b1213f8e2689bdf5e8
Barnaby Smith <barnaby@sabresaurus.com>
2021-07-04 20:52:00 +0100

    * Remove network code which has now been moved to a new repo https://github.com/sabresaurus/Remote-Actions

--- Editor/Helpers/TypeUtility.cs
+++ Editor/Helpers/TypeUtility.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using Sabresaurus.Sidekick.Requests;
 using UnityEngine.Assertions;
 
 namespace Sabresaurus.Sidekick
