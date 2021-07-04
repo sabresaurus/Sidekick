cb6619b8aef2597e41207e2f8f8715f9a3fba8c7
Barnaby Smith <barnaby@sabresaurus.com>
2021-07-04 20:55:35 +0100

    * Remove some unused code

--- Editor/PersistentData.cs
+++ Editor/PersistentData.cs
@@ -1,17 +1,14 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Sabresaurus.Sidekick
 {
-	/// <summary>
-	/// This class allows various utilities to store data that will persist through recompiles
-	/// </summary>
-	[System.Serializable]
-	public class PersistentData
-	{
-		// MethodPane
-		public List<MethodSetup> ExpandedMethods = new List<MethodSetup>();
-
-	}
+    /// <summary>
+    /// This class allows various utilities to store data that will persist through recompiles
+    /// </summary>
+    [System.Serializable]
+    public class PersistentData
+    {
+        // MethodPane
+        public List<MethodSetup> ExpandedMethods = new List<MethodSetup>();
+    }
 }
